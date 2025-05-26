b3f48b6b6268785a1f724ae86a7c974850dfc193
zyoni <zyoni@ymail.com>
2025-05-26 04:09:02 -0400

    Backend API

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,10 +23,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 
 const pinoHttp = require('pino-http');
