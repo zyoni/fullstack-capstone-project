269a5d7dbf70ed67c18acf0d107e6dc53aabb772
zyoni <zyoni@ymail.com>
2025-05-27 03:25:39 -0400

    Hands-on Lab: Add a Sentiment Analysis Service

--- sentiment/index.js
+++ sentiment/index.js
@@ -38,10 +38,10 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if (analysisResult > 0) {
-            sentiment = 'positive';
-        } else if (analysisResult < 0) {
-            sentiment = 'negative';
+        if (analysisResult < 0) {
+            sentiment = "negative";
+        } else if (analysisResult > 0.33) {
+            sentiment = "positive";
         }        
 
         // Logging the result
@@ -52,7 +52,7 @@ app.post('/sentiment', async (req, res) => {
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        return res.status(500).json({'message': 'Error performing sentiment analysis'});
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
 
