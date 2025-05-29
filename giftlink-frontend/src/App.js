2339b7bfba2aa7915eb48471bbaf2dfc941e5daf
zyoni <zyoni@ymail.com>
2025-05-29 05:19:36 -0400

    Hands-on Lab: Implement the Details Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,11 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+<<<<<<< HEAD
+=======
+import DetailsPage from './components/DetailsPage/DetailsPage';
+
+>>>>>>> bddfc90 (Hands-on Lab: Implement the Details Page)
 function App() {
   const navigate = useNavigate();
   return (
@@ -14,8 +19,12 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+<<<<<<< HEAD
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+=======
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
+>>>>>>> bddfc90 (Hands-on Lab: Implement the Details Page)
         </Routes>
         </>
   );
