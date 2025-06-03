c8973867ffd645eb47381cb530b6ca963b86513b
zyoni <zyoni@ymail.com>
2025-06-03 04:32:04 -0400

    Hands-on Lab: Develop Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,28 +22,18 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/gifts', giftRoutes);
-
-
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-const searchRoutes = require('./routes/searchRoutes');
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes);
-
-const authRoutes = require('./routes/authRoutes');
+app.use('/api/gifts', giftRoutes);
 app.use('/api/auth', authRoutes);
-
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,4 +1,3 @@
-//Step 1 - Task 2: Import necessary packages
 const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
@@ -8,27 +7,21 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
-
-//Step 1 - Task 3: Create a Pino logger instance
-dotenv.config();
-
 const logger = pino();  // Create a Pino logger instance
-
-//Step 1 - Task 4: Create JWT secret
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
-
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
         const db = await connectToDatabase();
         // Task 2: Access MongoDB collection
         const collection = db.collection("users");
-        // Task 3: Check for existing email
+        //Task 3: Check for existing email
         const existingEmail = await collection.findOne({ email: req.body.email });
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
-        // Task 4: Save user details in database
+        //Task 4: Save user details in database
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -36,14 +29,11 @@ router.post('/register', async (req, res) => {
             password: hash,
             createdAt: new Date(),
         });
-
-        // Task 5: Create JWT authentication with user._id as payload
         const payload = {
             user: {
                 id: newUser.insertedId,
             },
         };
-
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
         res.json({authtoken,email});
@@ -51,6 +41,4 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
-
 module.exports = router;
-
